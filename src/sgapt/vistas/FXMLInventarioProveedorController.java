410d1b9172142712b5ff154cbdfc9abdcfd38343
Sam <88214916+Samuelmtzp@users.noreply.github.com>
2023-05-11 00:51:40 -0600

    Creación de vistas

--- src/sgapt/vistas/FXMLInventarioProveedorController.java
+++ src/sgapt/vistas/FXMLInventarioProveedorController.java
@@ -13,12 +13,11 @@ import javafx.fxml.Initializable;
  *
  * @author samue
  */
-public class FXMLAdministracionInventarioProductosController implements Initializable {
+public class FXMLInventarioProveedorController implements Initializable {
 
     /**
-     * 
+     * Initializes the controller class.
      */
-    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
