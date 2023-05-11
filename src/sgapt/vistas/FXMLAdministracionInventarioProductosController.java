d40375f4a50ae4a5a69ccbbb2380dc8b6bc42cfc
Sam <88214916+Samuelmtzp@users.noreply.github.com>
2023-05-10 22:52:23 -0600

    Creación de archivos de vistas y controladores

---
+++ src/sgapt/vistas/FXMLAdministracionInventarioProductosController.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package sgapt.vistas;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author samue
+ */
+public class FXMLAdministracionInventarioProductosController implements Initializable {
+
+    /**
+     * Los productos de proveedores llegan en pedido, pero, los de otras sucursales también no?
+     * 
+     */
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
