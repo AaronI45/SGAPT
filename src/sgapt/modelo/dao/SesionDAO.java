86e9cb96c689e535caef472810fdf88d4f97935c
Sam <88214916+Samuelmtzp@users.noreply.github.com>
2023-05-12 02:03:45 -0600

    Conexión con la base de datos

--- src/sgapt/modelo/dao/SesionDAO.java
+++ src/sgapt/modelo/dao/SesionDAO.java
@@ -5,20 +5,20 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import sgapt.modelo.ConexionBD;
-import sgapt.modelo.pojo.Usuario;
+import sgapt.modelo.pojo.Empleado;
 import sgapt.util.Constantes;
 
 public class SesionDAO 
 {
-    public static Usuario verificarUsuarioSesion(String usuario, String password)
+    public static Empleado verificarUsuarioSesion(String usuario, String password)
     {
-        Usuario usuarioVerificado = new Usuario();
+        Empleado usuarioVerificado = new Empleado();
         Connection conexion = ConexionBD.abrirConexionBD();
         if (conexion != null)
         {
             try
             {
-                String consulta = "SELECT * FROM usuario WHERE username = ? AND password = ?";
+                String consulta = "SELECT * FROM empleado WHERE username = ? AND password = ?";
                 PreparedStatement prepararSentencia = conexion.prepareStatement(consulta);
                 prepararSentencia.setString(1, usuario);
                 prepararSentencia.setString(2, password);
@@ -26,10 +26,13 @@ public class SesionDAO
                 usuarioVerificado.setCodigoRespuesta(Constantes.OPERACION_EXITOSA);
                 if (resultado.next()) 
                 {
-                    usuarioVerificado.setIdUsuario(resultado.getInt("idUsuario"));
-                    usuarioVerificado.setNombre(resultado.getString("nombre"));
+                    usuarioVerificado.setIdEmpleado(resultado.getInt("idEmpleado"));
+                    usuarioVerificado.setFarmacia_idFarmacia(resultado.getInt("Farmacia_idFarmacia"));
+                    usuarioVerificado.setNombres(resultado.getString("nombres"));
                     usuarioVerificado.setApellidoPaterno(resultado.getString("apellidoPaterno"));
                     usuarioVerificado.setApellidoMaterno(resultado.getString("apellidoMaterno"));
+                    usuarioVerificado.setCorreoElectronico(resultado.getString("correoElectronico"));
+                    usuarioVerificado.setEsAdministrador(resultado.getInt("esAdministrador"));
                     usuarioVerificado.setUsername(resultado.getString("username"));
                     usuarioVerificado.setPassword(resultado.getString("password"));
                 }
--- src/sgapt/modelo/pojo/Empleado.java
+++ src/sgapt/modelo/pojo/Empleado.java
@@ -1,19 +1,119 @@
 package sgapt.modelo.pojo;
 
-import java.util.Date;
+public class Empleado {
 
-public class Empleado extends Persona {
+    private int idEmpleado;
+    private int Farmacia_idFarmacia;
+    private String nombres;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+    private String correoElectronico;
+    private int esAdministrador;
+    private String username;
+    private String password;
+    private int codigoRespuesta;
 
     public Empleado() {
     }
 
-    public Empleado(String nombre, String apellidoPaterno, String apellidoMaterno, int idPersona, Date fechaNacimiento, String correo, String direccion, byte[] foto) {
-        super(nombre, apellidoPaterno, apellidoMaterno, idPersona, fechaNacimiento, correo, direccion, foto);
+    public Empleado(int idEmpleado, int Farmacia_idFarmacia, String nombres, String apellidoPaterno, String apellidoMaterno, String correoElectronico, int esAdministrador, String username, String password, int codigoRespuesta) {
+        this.idEmpleado = idEmpleado;
+        this.Farmacia_idFarmacia = Farmacia_idFarmacia;
+        this.nombres = nombres;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+        this.correoElectronico = correoElectronico;
+        this.esAdministrador = esAdministrador;
+        this.username = username;
+        this.password = password;
+        this.codigoRespuesta = codigoRespuesta;
     }
 
+    public int getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(int idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public int getFarmacia_idFarmacia() {
+        return Farmacia_idFarmacia;
+    }
+
+    public void setFarmacia_idFarmacia(int Farmacia_idFarmacia) {
+        this.Farmacia_idFarmacia = Farmacia_idFarmacia;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public int getEsAdministrador() {
+        return esAdministrador;
+    }
+
+    public void setEsAdministrador(int esAdministrador) {
+        this.esAdministrador = esAdministrador;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getCodigoRespuesta() {
+        return codigoRespuesta;
+    }
+
+    public void setCodigoRespuesta(int codigoRespuesta) {
+        this.codigoRespuesta = codigoRespuesta;
+    }
+
+    
+    
     @Override
     public String toString() {
-        return "Empleado{" + super.toString();
+        return nombres+" "+apellidoPaterno+" "+apellidoMaterno;
     }
     
 }
