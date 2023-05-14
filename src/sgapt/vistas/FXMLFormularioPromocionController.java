2bdb593970848768dac56455678bfa50348d3f5b
LuisElizaldeX <king_mbea@hotmail.com>
2023-05-14 01:38:42 -0600

    Correccion de error
    correccion de error

--- src/sgapt/vistas/FXMLFormularioPromocionController.java
+++ src/sgapt/vistas/FXMLFormularioPromocionController.java
@@ -6,11 +6,16 @@ import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import static jdk.nashorn.internal.objects.NativeRegExp.source;
+import sgapt.modelo.dao.ProductoDAO;
+import sgapt.modelo.dao.ProductoRespuesta;
 import sgapt.modelo.dao.PromocionDAO;
 import sgapt.modelo.pojo.Producto;
 import sgapt.modelo.pojo.Promocion;
@@ -37,6 +42,8 @@ public class FXMLFormularioPromocionController implements Initializable {
     private Label lbErrorIdPromocion;
     @FXML
     private ComboBox<Producto> cbIdProducto;
+    @FXML
+    private Button clicBtnCancelar;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -74,14 +81,18 @@ public class FXMLFormularioPromocionController implements Initializable {
         registrarPromocion(promocionValidada);
     }
 
-    @FXML
+    @FXML 
     private void clicBtnCancelar(ActionEvent event) {
-        cerrarVentana();
+        Node source = (Node) event.getSource();
+        Stage stagePrincipal = (Stage) source.getScene().getWindow();
+        stagePrincipal.setScene(Utilidades.inicializarEscena("vistas/FXMLMAdministracionFormulario.fxml"));
+        stagePrincipal.setTitle("Home");
+        stagePrincipal.show();
     }
     
     private void cerrarVentana(){
-        Stage escenarioBase = (Stage) lbTitulo.getScene().getWindow();
-        escenarioBase.close();
+        Stage stagePrincipal = (Stage) lbTitulo.getScene().getWindow();
+        stagePrincipal.close();
     } 
     
     private void registrarPromocion(Promocion promocionRegistro){
@@ -105,7 +116,7 @@ public class FXMLFormularioPromocionController implements Initializable {
     
     /*private void cargarInformacionProducto(int idProducto){
         producto = FXCollections.observableArrayList();
-        ProductpRespuesta productoBD=ProductoDAO.obtenerInformacionProducto(idProducto);
+        ProductoRespuesta productoBD=ProductoDAO.obtenerInformacionProducto(idProducto);
         switch(productoBD.getCodigoRespuesta()){
             case Constantes.ERROR_CONEXION:
                 Utilidades.mostrarDialogoSimple("Error de conexion", "Error en la conexion con la base de datos", 
