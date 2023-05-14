54a13099e915548cc9a5a99853a7f8b5eeb9a683
AaronI45 <zs20015694@estudiantes.uv.mx>
2023-05-14 17:45:51 -0600

    quitar librerias

--- src/sgapt/controladores/FXMLInicioSesionController.java
+++ src/sgapt/controladores/FXMLInicioSesionController.java
@@ -8,8 +8,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
-import javafx.scene.Node;
-import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
