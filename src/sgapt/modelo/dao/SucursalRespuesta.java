7a290099f3ca4e3c4c1dbf4ee26ae92dbf2f797e
AaronI45 <92239980+AaronI45@users.noreply.github.com>
2023-05-13 21:04:53 -0600

    Agregar DAOS de sucursal y de producto

---
+++ src/sgapt/modelo/dao/SucursalRespuesta.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sgapt.modelo.dao;
+
+import java.util.ArrayList;
+import sgapt.modelo.pojo.Sucursal;
+
+/**
+ *
+ * @author super
+ */
+public class SucursalRespuesta {
+    private int codigoRespuesta;
+    private ArrayList<Sucursal> sucursales = new ArrayList<>();
+
+    public SucursalRespuesta() {
+    }
+    
+    public SucursalRespuesta(int codigoRespuesta) {
+        this.codigoRespuesta = codigoRespuesta;
+    }
+    
+    public int getCodigoRespuesta() {
+        return codigoRespuesta;
+    }
+
+    public void setCodigoRespuesta(int codigoRespuesta) {
+        this.codigoRespuesta = codigoRespuesta;
+    }
+
+    public ArrayList<Sucursal> getSucursales() {
+        return sucursales;
+    }
+
+    public void setSucursales(ArrayList<Sucursal> sucursales) {
+        this.sucursales = sucursales;
+    }
+    
+    
+}
